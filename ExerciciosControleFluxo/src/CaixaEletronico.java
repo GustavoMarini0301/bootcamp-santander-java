0cb32f774f379054f2e622053b547ce1a5a4e721
Gustavo Marini Fernandes <gustavomarinifernandes@gmail.com>
2024-05-16 19:10:13 -0300

    up

---
+++ ExerciciosControleFluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+//caixa eletronico
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 22.0;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("novo saldo\n" + saldo);
+        }
+        else
+        System.out.println("saldo insuficiente");
+    }
+}
---
+++ ExerciciosControleFluxo/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+// ResultadoEscolar.java
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 5;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota <7)//true or false
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+    }
+}
